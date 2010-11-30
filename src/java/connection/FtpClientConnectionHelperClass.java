2ced7bce9d8babb8b0a708c555981dd7721c94b0
geyzus <nagy.geza87@gmail.com>
2010-11-30 08:52:11 +0100

    Added Exception handler to FTP Connection handling

--- src/java/connection/FtpClientConnectionHelperClass.java
+++ src/java/connection/FtpClientConnectionHelperClass.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package connection;
 
 import java.io.IOException;
@@ -17,22 +16,28 @@ public class FtpClientConnectionHelperClass {
 
     FTPClient client = null;
 
-    public void connectClient(String username, String password, String url) throws SocketException, IOException{
-        client = new FTPClient();
-        client.connect(url);
-        client.login(username, password);
+    public void connectClient(String username, String password, String url) {
+        try {
+            client = new FTPClient();
+            client.connect(url);
+            client.login(username, password);
+        } catch (Exception e) {
+            e.printStackTrace();  
+        }
 
     }
 
-    public void diconnectClient() throws IOException{
-        if (client.isConnected()==true){
-            client.disconnect();
-        } else {
-            if (client!= null){
-                client = null;
+    public void diconnectClient() {
+        try {
+            if (client.isConnected() == true) {
+                client.disconnect();
+            } else {
+                if (client != null) {
+                    client = null;
+                }
             }
+        } catch (Exception e){
+            e.printStackTrace();
         }
     }
-
-
 }
